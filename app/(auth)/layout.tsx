bdaf1f4f727f8144d816e75b232bb239f0b51d65
jamez18 <jamesvilmaro@gmail.com>
2025-04-11 15:40:35 +0200

    new file:   app/(auth)/layout.tsx 	new file:   app/(auth)/login/page.tsx 	new file:   app/(dashboard)/dashboard/layout.tsx 	new file:   app/(dashboard)/dashboard/page.tsx 	new file:   app/(main)/layout.tsx 	new file:   app/(main)/page.tsx 	modified:   app/globals.css 	modified:   app/layout.tsx 	deleted:    app/page.tsx 	new file:   components.json 	new file:   components/header.tsx 	new file:   components/ui/button.tsx 	new file:   components/ui/navigation-menu.tsx 	new file:   components/ui/sheet.tsx 	new file:   lib/utils.ts 	modified:   package-lock.json 	modified:   package.json

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from 'react'
+
+export default function authenticationLayout({children}:
+    {children:ReactNode}) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
---
+++ app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from 'react'
+
+export default function dashboardLayout({children}:
+    {children:ReactNode}) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
